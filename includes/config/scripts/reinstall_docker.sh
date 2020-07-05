9907d3652436ad2a9a53c436cca020bf7e109d1f
root <laster13@hotmail.com>
2020-07-05 17:22:25 +0200

    Add reinstall_docker.sh

---
+++ includes/config/scripts/reinstall_docker.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+source includes/functions.sh
+source includes/variables.sh
+
+## Variable
+ansible-playbook /opt/seedbox-compose/includes/dockerapps/templates/ansible/ansible.yml
+SEEDUSER=$(cat /tmp/name)
+DOMAIN=$(cat /tmp/domain)
+SEEDGROUP=$(cat /tmp/group)
+rm /tmp/name /tmp/domain /tmp/group
+SERVICESPERUSER="$SERVICESUSER$SEEDUSER"
+
+    	echo -e "${CRED}------------------------------------------------------------------------------${CEND}"
+    	echo -e "${CCYAN}    /!\ Réinstallation Docker - traefik - Réinitialisation des Apllis /!\    ${CEND}"
+    	echo -e "${CRED}------------------------------------------------------------------------------${CEND}"
+	echo ""
+
+## Désinstallation Docker
+apt-get remove --purge docker*
+rm -rf /var/lib/docker
+rm -rf /etc/docker
+apt-get autoremove && apt-get autoclean
+
+## Réinstallation docker
+ansible-playbook /opt/seedbox-compose/includes/config/roles/docker/tasks/main.yml
+
+## suppression des yml dans /opt/seedbox/conf
+rm /opt/seedbox/conf/* > /dev/null 2>&1
+
+## reinstallation traefik, portainer
+install_traefik
+install_portainer
+
+echo ""
+## reinstallation watchtower
+install_watchtower
+echo ""
+
+## reinstallation application
+echo -e "${BLUE}### REINITIALISATION DES APPLICATIONS ###${NC}"
+echo -e " ${BWHITE}* Les fichiers de configuration ne seront pas effacés${NC}"
+while read line; do echo $line | cut -d'.' -f1; done < /home/$SEEDUSER/resume > $SERVICESPERUSER
+rm /home/$SEEDUSER/resume
+install_services
+ansible-vault encrypt /opt/seedbox/variables/account.yml > /dev/null 2>&1
+
+rm $SERVICESUSER$SEEDUSER
+    	echo -e "${CRED}---------------------------------------------------------------${CEND}"
+    	echo -e "${CRED}     /!\ MISE A JOUR DU SERVEUR EFFECTUEE AVEC SUCCES /!\      ${CEND}"
+    	echo -e "${CRED}---------------------------------------------------------------${CEND}"
+
+echo -e "\nAppuyer sur ${CCYAN}[ENTREE]${CEND} pour continuer..."
+read -r
+
