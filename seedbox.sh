87dd60a4d002fbde8ff415931bf82ab06c1228b4
Stéphane DEWITTE <stephane.dewitte@gmail.com>
2020-11-13 11:10:21 +0100

    Mise à jour des status au lancement

--- seedbox.sh
+++ seedbox.sh
@@ -292,6 +292,14 @@ EOF
   esac
 
 fi
+if [ ! -d "/opt/seedbox/status" ]
+then
+  mkdir -p /opt/seedbox/status
+fi
+for i in $(docker ps --format "{{.Names}}")
+do
+  echo "2" > /opt/seedbox/status/${i}
+done
 PLEXDRIVE="/usr/bin/rclone"
 if [[ -e "$PLEXDRIVE" ]]; then
   script_plexdrive
