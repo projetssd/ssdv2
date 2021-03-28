919f15d370dfef10e66202fabab8d2d38759e454
Merrick28 <stephane.dewitte@gmail.com>
2021-03-28 17:27:19 +0200

    reste plexdrive à analyser

--- includes/functions.sh
+++ includes/functions.sh
@@ -2379,7 +2379,7 @@ function get_from_account_yml() {
   if [ -z "$temp_return" ]; then
     temp_return=notfound
   fi
-  if [ "$temp_return" == None ]; then
+  if [ "$temp_return" == "None" ]; then
     temp_return=notfound
   fi
   ansible-vault encrypt "${CONFDIR}/variables/account.yml" >/dev/null 2>&1
