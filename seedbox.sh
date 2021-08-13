784e981edf978d83ac64819e91dc8caf7a1f497d
Stéphane DEWITTE <stephane.dewitte@gmail.com>
2021-08-13 13:44:51 +0200

    avertissement si maj à faire

--- seedbox.sh
+++ seedbox.sh
@@ -288,6 +288,8 @@ EOF
   #update_logrotate
   git_branch=$(git rev-parse --abbrev-ref HEAD)
   if [ ${git_branch} == 'master' ]; then
+    cd /opt/seedboc-compose
+    git fetch
     current_hash=$(git rev-parse HEAD)
     distant_hash=$(git rev-parse master@{upstream})
     if [ ${current_hash} != ${distant_hash} ]; then
