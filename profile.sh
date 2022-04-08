70a72240888f34c1e59ff83eded9879a8e7615fe
Merrick28 <stephane.dewitte@gmail.com>
2022-04-08 11:22:54 +0200

    variabilisation de /opt/seedbox-compose

--- profile.sh
+++ profile.sh
@@ -5,11 +5,12 @@ export PATH="$HOME/.local/bin:$PATH"
 if [ -f "${SCRIPTPATH}/ssddb" ]; then
     export BASEDIR="/opt/seedbox-compose"
     export CONFDIR="/opt/seedbox"
+    source ${SCRIPTPATH}/venv/bin/activate
 
     source ${SCRIPTPATH}/includes/variables.sh
     source ${SCRIPTPATH}/includes/functions.sh
     source ${SCRIPTPATH}/includes/menus.sh
-    source ${SCRIPTPATH}/venv/bin/activate
+
 
     PYTHONPATH=/opt/seedbox-compose/venv/lib/$(ls /opt/seedbox-compose/venv/lib)/site-packages
     export PYTHONPATH
